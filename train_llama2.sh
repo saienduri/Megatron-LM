73c64dc20251c30839a5d1eb9a1b4bcdf85dbdb6
lhcalibur <lhcalibur@users.noreply.github.com>
2024-06-21 15:33:39 +0800

    Update train_llama2.sh

--- train_llama2.sh
+++ train_llama2.sh
@@ -201,7 +201,7 @@ if __name__ == "__main__":
 
     with open(args.filename) as f:
         lines = f.readlines()
-    lines = lines[1:-1]
+    lines = lines[3:-1]
     lines = [float(a) for a in lines]
     mean = np.mean(np.array(lines))
     print(mean)' > mean_log_value.py
