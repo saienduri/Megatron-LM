cffbc41543cd0810d58750b1e4757c6df6e18572
Jiang Liu <jiangliu@amd.com>
2024-09-04 18:45:29 -0700

    use random port number

--- slurm_job_databrick.sh
+++ slurm_job_databrick.sh
@@ -12,6 +12,7 @@
 
 
 head_node=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+master_port=$((20000 + $RANDOM % 40000))
 #echo "Master Address: $master_addr"
 
 head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
@@ -36,5 +37,5 @@ export NCCL_IB_HCA=mlx5_0,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_7,mlx5_8,mlx5_9
 
 # NODE_RANK=0 NODE_RANK=1 NODE_RANK=2 NODE_RANK=3
 # nodes=1, nodes=2, nodes=3, nodes=4
-srun -l apptainer exec --bind /mnt/m2m_nobackup/yushengsu:/mnt/m2m_nobackup/yushengsu:rw,$HOME:$HOME:rw $HOME/apptainer_built_images/rocm_pytorch_private_exec_dash_pretuned_nightly_inai_FA_ck_v0_1_1_TE.sif bash tune_basetrain_databrick.sh MBS=5 BS=80 TP=1 PP=1 MODEL_SIZE=8 NO_TORCH_COMPILE=0 MASTER_ADDR=$head_node_ip NNODES=$SLURM_NNODES 
+srun -l apptainer exec --bind /mnt/m2m_nobackup/yushengsu:/mnt/m2m_nobackup/yushengsu:rw,$HOME:$HOME:rw $HOME/apptainer_built_images/rocm_pytorch_private_exec_dash_pretuned_nightly_inai_FA_ck_v0_1_1_TE.sif bash tune_basetrain_databrick.sh MBS=5 BS=80 TP=1 PP=1 MODEL_SIZE=8 NO_TORCH_COMPILE=0 MASTER_ADDR=$head_node_ip NNODES=$SLURM_NNODES MASTER_PORT=$master_port
 
